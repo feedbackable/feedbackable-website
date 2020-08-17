7d8c9ab4a31b99650b9f8b7271163ce227dc70ff
Seb Toombs <sebtoombs@gmail.com>
2020-08-17 19:08:23 +1000

    Add tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,15 +6,43 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    // {
+    //   resolve: `gatsby-plugin-gtag`,
+    //   options: {
+    //     // your google analytics tracking id
+    //     trackingId: `UA-175497827-1`,
+    //     // Puts tracking script in the head instead of the body
+    //     head: false,
+    //     // enable ip anonymization
+    //     anonymize: true,
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-gtag`,
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        // your google analytics tracking id
-        trackingId: `UA-175497827-1`,
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // enable ip anonymization
-        anonymize: true,
+        id: "GTM-5PT5G67",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: "route-change",
       },
     },
     {
