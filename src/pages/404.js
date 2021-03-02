9b8e89fabd43aa332ff41d9b7845eb451c40262d
Seb <sebtoombs@gmail.com>
2021-03-02 11:40:59 +1100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,12 +4,10 @@ import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import Card from "../components/Card"
 import { Text, Container } from "@chakra-ui/react"
-import Background from "../components/Background"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <Background display={["none", null, "block"]} />
     <Container py={[10, null, 20]}>
       <Card p="6" maxW="2xl" mx="auto">
         <Text as="h1" fontSize="3xl" mb="6">
