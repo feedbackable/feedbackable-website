77a573d24a3968c6e86ada565b176325e16f0c5e
Seb <sebtoombs@gmail.com>
2021-03-02 11:15:33 +1100

    Add manifest, update icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,9 +48,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `lato`
-        ],
+        fonts: [`lato`],
         display: "swap",
       },
     },
@@ -71,18 +69,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Feedbackable`,
+        short_name: `feedbackable`,
+        start_url: `/`,
+        background_color: `#943BDC`,
+        theme_color: `#943BDC`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
